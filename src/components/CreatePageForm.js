95182a356edf10248179c6f4f4fdd7954eb2eb28
Jess Mitchell <jess@jessmitchell.ca>
2023-04-06 13:25:28 -0400

    create posts through UI

--- src/components/CreatePageForm.js
+++ src/components/CreatePageForm.js
@@ -1,13 +1,20 @@
 import { createPage } from "@/lib/notion";
+import { useRouter } from "next/router";
 
 // No longer being used but leaving here for reference
 
 const handleSubmit = async (e) => {
   const pageName = e.target.name.value;
-  const result = await createPage(pageName);
+  const content = e.target.content.value;
+  const result = await createPage({ name: pageName, content });
   // do something with result, like show success snackbar
 };
-export const CreatePageForm = ({ refreshData }) => {
+export const CreatePageForm = () => {
+  const router = useRouter();
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
+
   const onSubmit = async (e) => {
     e.preventDefault();
     await handleSubmit(e);
@@ -15,16 +22,23 @@ export const CreatePageForm = ({ refreshData }) => {
   };
   return (
     <section>
-      <h3>Create a new page</h3>
+      <h3>Create a new blog post in your database</h3>
       <form onSubmit={onSubmit}>
         <label htmlFor="name">
           Page name:
+          <br />
           <input type="text" id="name" />
         </label>
+        <br />
+        <label htmlFor="content">
+          Content:
+          <br />
+          <textarea name="content" id="content" cols="30" rows="10"></textarea>
+        </label>
         <input
-          class="submit-input"
+          className="submit-input"
           type="submit"
-          value="Create new Notion page"
+          value="Add new post to database"
         />
       </form>
     </section>
